86fe7cac9a0ba2dd899c1fff325b422b540f7a7a
Todd Albert <todd@bocacode.com>
2022-07-25 16:48:46 -0400

    added post request and dynamic get route

--- index.js
+++ index.js
@@ -3,6 +3,14 @@ import express from 'express';
 
 // initialize express server
 const app = express();
+app.use(express.json());
+
+// add a new student (in theory)
+app.post('/students', (request, response) => {
+  const newStudent = request.body;
+  students.push(newStudent);
+  response.status(201).send(students);
+});
 
 // list requests that we want express to respond to
 app.get('/hello', (request, response) => {
@@ -10,6 +18,26 @@ app.get('/hello', (request, response) => {
   response.send('Hello World 🌎!');
 });
 
+const students = [
+  { firstName: 'Jonathan', lastName: 'Vegas' },
+  { firstName: 'Bridgette', lastName: 'Lemus' },
+  { firstName: 'Mason', lastName: 'Madaras' },
+  { firstName: 'Sebastien', lastName: 'Placide' },
+  { firstName: 'Logan', lastName: 'McCalley' },
+];
+
+app.get('/students', (request, response) => {
+  response.send(students);
+});
+// /students/Mason
+app.get('/students/:fName', (request, response) => {
+  const student = students.find(stud => stud.firstName === request.params.fName);
+  if(!student) {
+    response.status(404).send({ message: 'Student not found', success: false });
+    return;
+  }
+  response.send(student);
+});
 
 // start listening on a port
 app.listen(3001, () => {
